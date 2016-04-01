27ad7fb3e27bebf635ca7aa2db9b1f9c7b0490e5
Brad Hughes <bradleythughes@gmail.com>
2016-04-01 15:41:11 -0400

    added x's and o's

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   var $gameCells = $('.cell');
+  var moves = ["", "", "", "", "", "", "", "", ""];
+  var count = 0;
+  var turn = 'X'
 
-  $($gameCells).one('click', function(){
-    console.log(this.id);
-  });
+  $($gameCells).one('click', function() {
+    $(this).html(turn);
+    moves[this.id] = this.id;
+    count++;
+    moves[this.id] = (count % 2 == 0) ? turn ='X' : turn = 'O';
+    console.log(moves, count);
+
+  })
 
 });
