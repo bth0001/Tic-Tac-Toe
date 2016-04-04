fb52d4e83a970f8b05a681b6754d2694556556fa
Brad Hughes <bradleythughes@gmail.com>
2016-04-04 11:50:30 -0400

    fixed getWinner function, now need to fix draw function

--- main.js
+++ main.js
@@ -1,17 +1,84 @@
 $(document).ready(function() {
-
-  var $gameCells = $('.cell');
+  var auburn = '<img src="http://i.imgur.com/nGtYCT6.gif">'
+  var bama = '<img src="http://i.imgur.com/ETEHO8Q.jpg">'
+  var cell = $('.cell');
+  var resetBtn = $('#reset-btn');
   var moves = ["", "", "", "", "", "", "", "", ""];
   var count = 0;
   var turn = 'X'
 
-  $($gameCells).one('click', function() {
-    $(this).html(turn);
-    moves[this.id] = this.id;
-    count++;
-    moves[this.id] = (count % 2 == 0) ? turn ='X' : turn = 'O';
-    console.log(moves, count);
+for (var i = 0; i < cell.length; i++){
+  cell[i].addEventListener('click', setCount);
+}
 
-  })
+resetBtn.addEventListener('click', resetBoard);
+
+function setCount(){
+  if(this.innerHTML !== ''){
+    alert('Pick another Square')
+  }else if(count % 2 == 0){
+    count += 1;
+    this.innerHTML = auburn;
+    getWinner();
+  }else {
+    count += 1;
+    this.innerHTML = bama;
+    getWinner();
+  }
+}
+
+function getWinner(){
+  if(cell[0].innerHTML === auburn && cell[1].innerHTML === auburn && cell[2].innerHTML === auburn){
+    alert('Auburn Wins!');
+  }else if(cell[3].innerHTML === auburn && cell[4].innerHTML  === auburn && cell[5].innerHTML === auburn){
+    alert('Auburn Wins!');
+  }else if(cell[6].innerHTML  === auburn && cell[7].innerHTML  === auburn && cell[8].innerHTML === auburn){
+    alert('Auburn Wins!')
+  }
+
+   else if(cell[0].innerHTML  === auburn && cell[3].innerHTML  === auburn && cell[6].innerHTML === auburn){
+    alert('Auburn Wins!');
+  }else if(cell[1].innerHTML === auburn && cell[4].innerHTML === auburn && cell[7].innerHTML === auburn){
+    alert('Auburn Wins!');
+  }else if(cell[2].innerHTML === auburn && cell[5].innerHTML === auburn && cell[8].innerHTML === auburn){
+    alert('Auburn Wins!');
+  }
+
+   else if(cell[0].innerHTML === auburn && cell[4].innerHTML === auburn && cell[8].innerHTML === auburn){
+    alert('Auburn Wins!');
+  }else if(cell[6].innerHTML === auburn && cell[4].innerHTML === auburn && cell[2].innerHTML === auburn){
+    alert('Auburn Wins!');
+  }
+
+   else if(cell[0].innerHTML === bama && cell[1].innerHTML === bama && cell[2].innerHTML === bama){
+    alert('Bama Wins!');
+  }else if(cell[3].innerHTML === bama && cell[4].innerHTML  === bama && cell[5].innerHTML === bama){
+    alert('Bama Wins!');
+  }else if(cell[6].innerHTML  === bama && cell[7].innerHTML  === bama && cell[8].innerHTML === bama){
+    alert('Bama Wins!')
+  }
+
+   else if(cell[0].innerHTML  === bama && cell[3].innerHTML  === bama && cell[6].innerHTML === bama){
+    alert('Bama Wins!');
+  }else if(cell[1].innerHTML === bama && cell[4].innerHTML === bama && cell[7].innerHTML === bama){
+    alert('Bama Wins!');
+  }else if(cell[2].innerHTML === bama && cell[5].innerHTML === bama && cell[8].innerHTML === bama){
+    alert('Bama Wins!');
+  }
+
+   else if(cell[0].innerHTML === bama && cell[4].innerHTML === bama && cell[8].innerHTML === bama){
+    alert('Bama Wins!');
+  }else if(cell[6].innerHTML === bama && cell[4].innerHTML === bama && cell[2].innerHTML === bama){
+    alert('Bama Wins!');
+  }
+
+function resetBoard(){
+  for(var i = 0; i < cell.length; i++){
+    cell[i].innerHTML = '';
+  }
+  count = 0;
+}
+
+}
 
 });
